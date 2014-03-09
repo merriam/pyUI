d3bab00ea43895251b378440e14c04c2f087789c
Charles Merriam <charles.merriam@gmail.com>
2014-03-09 14:51:03 -0700

    tk_ok.py now quits on close button.

--- src/tk_ok.py
+++ src/tk_ok.py
@@ -7,6 +7,7 @@ from tkinter import Tk, BOTH, RIGHT, RAISED
 # in tkinter.constants
 # Tk is the root window, Frame is a container
 # tkinter.Frame != tkinter.ttk.Frame.  For example ttk has no background attribute
+# It is always a bad design choice to have two classes that almost, but not quite, identical
 from tkinter.ttk import Frame, Button, Style
 
 
@@ -25,27 +26,36 @@ class Example(Frame):
 
         self.style = Style()
         self.style.theme_use("default")
-        # styles
+        # styles are 'clam' 'default' 'alt' or 'classic', but so far seem the same
 
-        # frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame = Frame(self, relief=RAISED, borderwidth=1)
         frame = Frame(self)
         frame.pack(fill=BOTH, expand=1)
         # this frame has nothing, but pushes buttons down
 
         self.pack(fill=BOTH, expand=1)
 
-        closeButton = Button(self, text="Close")
+        closeButton = Button(self, text="Close", command=self.quit)
         closeButton.pack(side=RIGHT, padx=5, pady=5)
         okButton = Button(self, text="OK")
         okButton.pack(side=RIGHT)
         self.pack(fill=BOTH, expand=1)
         # pack geometry manager with expand in both directions
 
+    def center_main_window(self, width=290, height=150):
+        screen_width = self.parent.winfo_screenwidth()
+        screen_height = self.parent.winfo_screenheight()
+
+        new_x = (screen_width - width) // 2
+        new_y = (screen_height - height) // 2
+        self.parent.geometry('{}x{}+{}+{}'.format(width, height, new_x, new_y))
+
 def main():
     print("Running")
     root = Tk()
-    root.geometry("250x150+300+300")
+    #root.geometry("250x150+300+300")
     app = Example(root)
+    app.center_main_window()
     root.mainloop()
     print("Ran.")
 
